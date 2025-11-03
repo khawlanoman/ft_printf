8db7ede6681f97d2ce48e50133875f7e830e677c
noma <125211901+khawlanoman@users.noreply.github.com>
2025-11-03 18:54:21 +0100

    Refactor base16 functions and update pointer handling

--- helper2_ft.c
+++ helper2_ft.c
@@ -6,24 +6,22 @@
 /*   By: khnoman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 09:52:02 by khnoman           #+#    #+#             */
-/*   Updated: 2025/10/29 11:00:49 by khnoman          ###   ########.fr       */
+/*   Updated: 2025/10/31 09:57:11 by khnoman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 ////////////ft_base16_lower///////////
-
-int	ft_base16_lower(unsigned long n)
+int	ft_base16_lower(unsigned int n)
 {
 	int		count;
-	char	str[16];
+	char	*str;
 
 	count = 0;
-	str[16] = "0123456789abcdef";
+	str = "0123456789abcdef";
 	if (n >= 16)
 		count += ft_base16_lower(n / 16);
-	ft_putchar(str[n % 16]);
-	count++;
+	count +=ft_putchar(str[n % 16]);
 	return (count);
 }
 ////////////ft_base16_upper///////////
@@ -31,30 +29,40 @@ int	ft_base16_lower(unsigned long n)
 int	ft_base16_upper(unsigned int n)
 {
 	int		count;
-	char	str[16];
+	char	*str1;
 
-	conut = 0;
-	str[16] = "0123456789ABCDEF";
+	count = 0;
+	str1 = "0123456789ABCDEF";
 	if (n >= 16)
 		count += ft_base16_upper(n / 16);
-	ft_putchar(str[n % 16]);
+	ft_putchar(str1[n % 16]);
 	count++;
 	return (count);
 }
 
 /////////ft_putptr for pointer///////////////
 
+int	ft_base16_ptr(unsigned long n)
+{
+	int		count;
+	char	*str;
+
+	count = 0;
+	str = "0123456789abcdef";
+	if (n >= 16)
+		count += ft_base16_ptr(n / 16);
+	count +=ft_putchar(str[n % 16]);
+	return (count);
+}
+
 int	ft_putptr(void *ptr)
 {
-	int				count;
-	unsigned long	p;
+	int count;
 
 	count = 0;
 	if (!ptr)
-		return (0);
-	p = (unsigned long)ptr;
-	count += ft_putchar('0');
-	count += ft_putchar('x');
-	count += ft_base16_lower(p);
+		return(ft_putstr("(nil)"));
+	count += ft_putstr("0x");
+	count += ft_base16_ptr((unsigned long)ptr);
 	return (count);
 }
