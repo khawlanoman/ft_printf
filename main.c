9725ad9a59f39510f473e61d3f1c29164e80b1ae
noma <125211901+khawlanoman@users.noreply.github.com>
2025-11-04 16:07:28 +0100

    Add main function with printf examples

---
+++ main.c
@@ -0,0 +1,65 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%r");
+    // int ret1, ret2;
+
+    // // ======= Characters =======
+    // ret1 = ft_printf("Char: %c\n", 'A');
+    // ret2 = printf("Char: %c\n", 'A');
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Strings =======
+    // ret1 = ft_printf("String: %s\n", "Hello World");
+    // ret2 = printf("String: %s\n", "Hello World");
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Pointers =======
+    // int x = 42;
+    // ret1 = ft_printf("Pointer: %p\n", &x);
+    // ret2 = printf("Pointer: %p\n", &x);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Decimal/Integer =======
+    // ret1 = ft_printf("Decimal: %d\n", 12345);
+    // ret2 = printf("Decimal: %d\n", 12345);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // ret1 = ft_printf("Negative: %i\n", -54321);
+    // ret2 = printf("Negative: %i\n", -54321);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Unsigned =======
+    // ret1 = ft_printf("Unsigned: %u\n", 4294967295u);
+    // ret2 = printf("Unsigned: %u\n", 4294967295u);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Hexadecimal =======
+    // ret1 = ft_printf("Hex lower: %x\n", 305441741);
+    // ret2 = printf("Hex lower: %x\n", 305441741);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // ret1 = ft_printf("Hex upper: %X\n", 305441741);
+    // ret2 = printf("Hex upper: %X\n", 305441741);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Percent sign =======
+    // ret1 = ft_printf("Percent: %%\n");
+    // ret2 = printf("Percent: %%\n");
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+    // // ======= Multiple arguments =======
+    // ret1 = ft_printf("Mix: %c %s %d %i %u %x %X %p %%\n", 'Z', "Test", -42, 42, 3000000000u, 255, 255, &x);
+    // ret2 = printf("Mix: %c %s %d %i %u %x %X %p %%\n", 'Z', "Test", -42, 42, 3000000000u, 255, 255, &x);
+    // ft_printf("ft_printf returned: %d | printf returned: %d\n\n", ret1, ret2);
+
+
+    // printf("Percent: %");
+    //printf("laaaaaa\n");
+    //ft_printf("dyali: %");
+    
+
+    return 0;
+}
